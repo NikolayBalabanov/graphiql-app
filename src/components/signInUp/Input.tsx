8633fc408989a0b7e6c2ccb874b8ad924e279898
viktorykings <kingsviktory@gmail.com>
2023-05-06 16:32:09 +0300

    feat: add validation

--- src/components/signInUp/Input.tsx
+++ src/components/signInUp/Input.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Path, UseFormRegister } from 'react-hook-form';
+import { Path, UseFormRegister, ValidationRule } from 'react-hook-form';
 interface InputProps{
     type: string,
     id: string,
     placeholder: string,
     register: UseFormRegister<FormFields>;
     required: boolean,
-    label: Path<FormFields>
+    label: Path<FormFields>,
+    validation: RegExp | string | number,
 }
 type FormFields = {
     email: string,
@@ -14,8 +15,18 @@ type FormFields = {
     name?: string,
   };
 
-export const Input = ({type, id, placeholder, label,  register, required}: InputProps) => {
+export const Input = ({type, id, placeholder, label,  register, required, validation}: InputProps) => {
+  const nameValidator = (val: string) => {
+    if(val.length < 3) return false
+  }
+
   return (
-    <input type={type} id={id} placeholder={placeholder} className="m-2 rounded-md" {...register(label, {required: true})} />
+    <input
+      type={type}
+      id={id}
+      placeholder={placeholder}
+      className="m-2 rounded-md"
+      {...register(label, {required: true})}
+    />
   )
 }
--- src/components/signInUp/SignInUp.tsx
+++ src/components/signInUp/SignInUp.tsx
@@ -14,10 +14,46 @@ export const SignInUp = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<FormFields>();
   const onSubmit: SubmitHandler<FormFields> = data => console.log(data);
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center self-center px-1 py-6 h-60 w-3/5 border rounded-md my-4 font-semibold">
-      {isSignUp && <Input type="text" id="name" placeholder="Ivan" label="name" register={register} required />}
-      <Input type="email" id="email" placeholder="example@ex.com" label="email" register={register} required />
-      <Input type="password" id="password" placeholder="Password" label="password" register={register} required />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col justify-center self-center px-1 py-6 h-60 w-3/5 border rounded-md my-4 font-semibold"
+      noValidate
+      >
+      {isSignUp &&
+                <input
+                type="text"
+                id="name"
+                placeholder="Ivan"
+                className="m-2 rounded-md"
+                {...register("name", {required: true, pattern:{            value: /[A-Z]{1}/,
+                message: 'Your name should start with uppercase letter'}})}
+              />
+}                  {isSignUp && errors.name?.message}
+
+      <input
+      type="email"
+      id="email"
+      placeholder="example@ex.com"
+      className="m-2 rounded-md"
+      {...register("email", {required: true, pattern:{
+        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        message:" Enter valid email"
+      }})}
+    />
+        {errors.email?.message}
+
+          <input
+      type="password"
+      id="password"
+      placeholder="Password"
+      className="m-2 rounded-md"
+      {...register("password", {required: true, pattern:{
+        value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/,
+        message: 'Your password should contain 1 uppercase letter, 1 lowercase letter, 1 digit, min length 8'
+      }})}
+    />
+    {errors.password?.message}
+
       <Button type="submit" text={isSignUp ? 'Sign Up' : 'Sign In'}></Button>
       {!isSignUp
        ? <p>
