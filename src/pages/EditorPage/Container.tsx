44f92a4a4eb10ec2b488140f5c27aa1cd3b7c8d1
viktorykings <kingsviktory@gmail.com>
2023-05-19 09:52:31 +0300

    fix: form in editor

--- src/pages/EditorPage/Container.tsx
+++ src/pages/EditorPage/Container.tsx
@@ -4,9 +4,9 @@ import { Response } from './Response'
 
 export const Container = () => {
   return (
-    <form className='ml-8 p-2 grow bg-BGcolor2 rounded-2xl flex'>
+    <div className='ml-8 p-2 grow bg-BGcolor2 rounded-2xl flex'>
         <Editor />
         <Response />
-    </form>
+    </div>
   )
 }
--- src/pages/EditorPage/Editor.tsx
+++ src/pages/EditorPage/Editor.tsx
@@ -4,12 +4,12 @@ import { Variables } from './Variables';
 
 export const Editor = () => {
   return (
-    <div className='bg-BGcolor w-1/2 rounded-2xl h-full p-2 flex flex-col justify-between'>
+    <form className='bg-BGcolor w-1/2 rounded-2xl h-full p-2 flex flex-col justify-between'>
         <textarea name="editor" id="editor" cols={30} rows={10} className='bg-BGcolor w-4/5 m-6 '></textarea>
         <div className='flex justify-evenly my-4'>
         <Headers />
         <Variables />
         </div>
-    </div>
+    </form>
   )
 }
